a0a3c5bdcf9b003eb13f3a502db9cbe13a1ee933
ziyagurel <ziyagurel55@hotmail.com>
2021-05-21 16:13:30 +0300

    Updated ProductDao

--- src/main/java/com/ziyagurel/restnorthwindh/entities/abstracts/IProductDao.java
+++ src/main/java/com/ziyagurel/restnorthwindh/entities/abstracts/IProductDao.java
@@ -3,6 +3,6 @@ package com.ziyagurel.restnorthwindh.entities.abstracts;
 import com.ziyagurel.restnorthwindh.entities.concretes.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductDao extends JpaRepository<Product,Integer> {
+public interface IProductDao extends JpaRepository<Product,Integer> {
 
 }
