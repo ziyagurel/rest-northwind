9957c4f49fc4b9e4247bf3c660df4c7b57aea42e
ziyagurel <ziyagurel55@hotmail.com>
2021-05-21 16:14:04 +0300

    Added services for product

--- src/main/java/com/ziyagurel/restnorthwindh/business/abstracts/IProductService.java
+++ src/main/java/com/ziyagurel/restnorthwindh/business/abstracts/IProductService.java
@@ -4,7 +4,7 @@ import com.ziyagurel.restnorthwindh.entities.concretes.Product;
 
 import java.util.List;
 
-public interface ProductService {
+public interface IProductService {
 
     List<Product> getAll();
 }
---
+++ src/main/java/com/ziyagurel/restnorthwindh/business/concretes/ProductManager.java
@@ -0,0 +1,24 @@
+package com.ziyagurel.restnorthwindh.business.concretes;
+
+import com.ziyagurel.restnorthwindh.business.abstracts.IProductService;
+import com.ziyagurel.restnorthwindh.entities.abstracts.IProductDao;
+import com.ziyagurel.restnorthwindh.entities.concretes.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductManager implements IProductService {
+    private IProductDao dao;
+
+    @Autowired
+    public ProductManager(IProductDao dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+}
