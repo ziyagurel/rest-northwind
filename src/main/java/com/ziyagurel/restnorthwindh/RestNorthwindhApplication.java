3cf7d1b9ab2a9751a3f4e02bcbe81a4244bb83a0
ziyagurel <ziyagurel55@hotmail.com>
2021-05-21 11:42:26 +0300

    Created hiararchy for spring project

---
+++ src/main/java/com/ziyagurel/restnorthwindh/RestNorthwindhApplication.java
@@ -0,0 +1,13 @@
+package com.ziyagurel.restnorthwindh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestNorthwindhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestNorthwindhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ziyagurel/restnorthwindh/RestNorthwindhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziyagurel.restnorthwindh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestNorthwindhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
